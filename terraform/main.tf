f6f3e2d3f37da874f1174c7094770f4dc6c1ff2c
Taosuan <ys.taosuan@outlook.jp>
2026-04-01 15:40:25 +0900

    Add files via upload

---
+++ terraform/main.tf
@@ -0,0 +1,90 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-northeast-1"
+  access_key = ""
+  secret_key = ""
+  //AWSコンソール画面右上の自身のアカウント名をクリックし、「セキュリティ認証情報」配下の「アクセスキーを作成」を押下し作成
+}
+
+# S3 Bucket
+resource "aws_s3_bucket" "site" {
+  bucket = "taosuan-static-site-20260401"
+}
+
+# パブリックアクセス完全遮断
+resource "aws_s3_bucket_public_access_block" "site" {
+  bucket = aws_s3_bucket.site.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# CloudFront OAC
+resource "aws_cloudfront_origin_access_control" "oac" {
+  name                              = "s3-oac"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+# -------------------
+# CloudFront
+# -------------------
+resource "aws_cloudfront_distribution" "cdn" {
+
+  origin {
+    domain_name = aws_s3_bucket.site.bucket_regional_domain_name
+    origin_id   = "s3-origin"
+
+    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    target_origin_id       = "s3-origin"
+    viewer_protocol_policy = "redirect-to-https"
+
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods  = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+# S3 Bucket Policy（CloudFrontのみ許可）
+resource "aws_s3_bucket_policy" "site_policy" {
+  bucket = aws_s3_bucket.site.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action = "s3:GetObject"
+        Resource = "${aws_s3_bucket.site.arn}/*"
+      }
+    ]
+  })
+}
